cce7977ad1d0c5d8838dc654b54dc45f69254ea2
HafidaBaazizi <149062372+HafidaBaazizi@users.noreply.github.com>
2026-03-15 15:13:08 +0000

    Update run_project.bat

--- run_project.bat
+++ run_project.bat
@@ -1,4 +1,7 @@
 @echo off
-python scriptsun_experiments.py --data-dir data	splib --output resultsaw_results.csv --runs 2 --seed 42 --solutions data	splib\solutions.txt
-python scripts\summarize_results.py --input resultsaw_results.csv --summary results\summary_results.csv --latex report	ables_generated_results.tex --figure-dir resultsigures
+python scripts
+un_experiments.py --data-dir data	splib --output results
+aw_results.csv --runs 5 --seed 42 --solutions data	splib\solutions.txt
+python scripts\summarize_results.py --input results
+aw_results.csv --summary results\summary_results.csv --latex report	ables_generated_results.tex --figure-dir resultsigures
 pause
